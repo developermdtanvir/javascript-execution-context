1d26d6a8dbdeacb89aac07d8929adee08598888c
mdtanvirgit <freelancermdtanvir@gmail.com>
2023-03-13 06:58:30 +0600

    I am addded all file

--- js/index.js
+++ js/index.js
@@ -1,5 +1,31 @@
-document.getElementById("submit").addEventListener('submit',(e)=>{
-    e.preventDefault()
-    const {name,email,color,date} = e.target
-    console.log(name.value,email.value,color.value,date.value);
-})  
+// problem number 1
+
+document.getElementById('input').addEventListener('click',()=>{
+    const userInput =  prompt("Enter Your Number")
+     const userNumber = parseInt(userInput);
+     if(userNumber){
+         const totalNumber = userNumber + 200;
+         return alert(`Your Number is ${totalNumber}`)
+     }
+ })
+
+ // problem number2
+ document.getElementById('location').addEventListener('click',()=>{
+    const isLocation = confirm('Can You get Location')
+    console.log(isLocation);
+    if(isLocation){
+        console.log(window.location.href);
+    }
+
+ })
+
+var counter = 0;
+
+function plus() {
+  counter += 1;
+  localStorage.setItem('counter',counter);
+  document.getElementById("counter").innerText = counter;
+}
+
+let value = parseInt(localStorage.getItem('counter'));
+document.getElementById("counter").innerText = value;
